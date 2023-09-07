aa8358df1505b2a464882d8d5d5c40073da1e260
sarahc-dev <sarahclements@hotmail.co.uk>
2023-09-07 22:20:16 +0100

    GET /todos passing green

--- api/src/index.spec.ts
+++ api/src/index.spec.ts
@@ -1,36 +1,40 @@
 import request from "supertest";
 import app, { server } from "./index";
+import Todo from "./models/todoModel";
 
-afterAll(() => {
+afterAll(async () => {
     server.close();
 });
 
+afterEach(async () => {
+    await Todo.deleteMany({})
+})
+
 describe("Todolist API", () => {
     it("GET /todos returns an array of all todos", async () => {
-        return request(app)
+        await new Todo({ title: "Feed cat" }).save()
+        const response = await request(app)
             .get("/todos")
+            .set("Accept", "application/json")
             .expect("Content-Type", /json/)
             .expect(200)
-            .then(response => {
-                expect(response.body).toEqual(
+           
+            expect(response.body).toEqual(
                     expect.arrayContaining([
                         expect.objectContaining({
-                            todo: expect.any(String),
+                            title: expect.any(String),
                             completed: expect.any(Boolean),
                         }),
                     ])
                 );
             });
-    });
-
-    it("GET /todos returns an empty array when there are no todos", () => {
-        return request(app)
+  
+    it("GET /todos returns an empty array when there are no todos", async () => {
+        const response = await request(app)
             .get("/todos")
             .expect("Content-Type", /json/)
             .expect(200)
-            .then(response => {
-                expect(response.body).toHaveLength(0)
-            });
+            
+        expect(response.body).toHaveLength(0)
     });
-    
 });
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import Todo from "./models/todoModel";
 
 const app = express();
 const port = 8080;
 
-app.get("/todos", (req, res) => {
-    res.json([{ todo: "Get milk", completed: true }]);
+const dbUrl = `mongodb://0.0.0.0/${process.env.NODE_ENV === "test" ? "todolist_test" : "todolist"}`
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get("/todos", async (req, res) => {
+    try {
+        const todos = await Todo.find()
+        res.status(200).json(todos)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
 });
 
 export const server = app.listen(port, () => {
