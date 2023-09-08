6b6c134477563ac178ba56e58326e161113570a0
sarahc-dev <sarahclements@hotmail.co.uk>
2023-09-08 12:12:35 +0100

    add test for error handling and db connection to increase coverage

--- api/src/index.spec.ts
+++ api/src/index.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
-import app, { server } from "./index";
+import app, { server, getDbUrl } from "./index";
 import Todo from "./models/todoModel";
+import mongoose from "mongoose";
 
 afterAll(async () => {
     server.close();
@@ -15,19 +16,14 @@ describe("Todolist API", () => {
         await new Todo({ title: "Feed cat" }).save()
         const response = await request(app)
             .get("/todos")
-            .set("Accept", "application/json")
             .expect("Content-Type", /json/)
             .expect(200)
            
-            expect(response.body).toEqual(
-                    expect.arrayContaining([
-                        expect.objectContaining({
-                            title: expect.any(String),
-                            completed: expect.any(Boolean),
-                        }),
-                    ])
-                );
-            });
+        expect(response.body).toHaveLength(1);
+        expect(response.body[0]).toHaveProperty("_id");
+        expect(response.body[0].title).toBe("Feed cat");
+        expect(response.body[0].completed).toBe(false);
+    });
   
     it("GET /todos returns an empty array when there are no todos", async () => {
         const response = await request(app)
@@ -37,4 +33,33 @@ describe("Todolist API", () => {
             
         expect(response.body).toHaveLength(0)
     });
+
+    it("GET /todos returns a 500 status error if cannot get todos", async () => {
+        const findSpy = jest.spyOn(Todo, 'find');
+        findSpy.mockImplementation(() => {
+            throw new Error('Simulated error')
+        })
+     
+        const response = await request(app)
+        .get("/todos")
+        .expect("Content-Type", /json/)
+        .expect(500)
+
+        expect(response.body).toEqual({ error: "Internal server error" })
+
+        findSpy.mockRestore()
+    })
 });
+
+describe("Database URL configuration", () => {
+    it("should use the test database when NODE_ENV='test'", () => {
+        const dbUrl = getDbUrl()
+        expect(dbUrl).toBe("mongodb://0.0.0.0/todolist_test")
+    })
+
+    it("should use the todolist database when NODE_ENV='production", () => {
+        process.env.NODE_ENV = 'production'
+        const dbUrl = getDbUrl()
+        expect(dbUrl).toBe("mongodb://0.0.0.0/todolist")
+    })
+})
--- api/src/index.ts
+++ api/src/index.ts
@@ -5,19 +5,19 @@ import Todo from "./models/todoModel";
 const app = express();
 const port = 8080;
 
-const dbUrl = `mongodb://0.0.0.0/${process.env.NODE_ENV === "test" ? "todolist_test" : "todolist"}`
+export const getDbUrl = () => `mongodb://0.0.0.0/${process.env.NODE_ENV === "test" ? "todolist_test" : "todolist"}`
 
-mongoose.connect(dbUrl);
+mongoose.connect(getDbUrl());
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/todos", async (req, res) => {
     try {
-        const todos = await Todo.find()
+        const todos = await Todo.find({})
         res.status(200).json(todos)
     } catch (error) {
-        console.error(error)
+        console.error("Error:", error)
         res.status(500).json({ error: 'Internal server error' })
     }
 });
