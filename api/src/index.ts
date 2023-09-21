3ab58d09899c916997b7d6763061cd19b7fa42b8
sarahc-dev <sarahclements@hotmail.co.uk>
2023-09-21 14:25:20 +0100

    add fetch request to get and post. e2e add new todo test passing green

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import todosRouter from "./routes/todosRouter";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/todos", todosRouter);
 
